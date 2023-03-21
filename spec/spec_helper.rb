7a6a06b466c0642e9643ffeb1163f1fe97e184af
tanamaru <yt.0408.mmm@gmail.com>
2023-03-21 23:22:51 +0900

    requireの変更

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,9 +16,17 @@
 
 require 'capybara/rspec'
 
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless
+    Capybara.javascript_driver = :selenium_chrome_headless
   end
   
   # rspec-expectations config goes here. You can use an alternate
