2d7d872de33d4a7781364dddf7ede71cb40ccc32
tanamaru <yt.0408.mmm@gmail.com>
2023-04-05 23:23:58 +0900

    ajaxの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,9 +33,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    @task = current_user.tasks.find(params[:id])
+    @task.destroy
+    head :no_content
   end
 
   private
